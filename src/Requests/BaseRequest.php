e4d4c7ea18e346a9dd481131184a256625b0f899
jadessoriano <jadesiguasoriano@gmail.com>
2025-04-19 16:00:20 +0800

    fix: calling unknown methods

--- src/Requests/BaseRequest.php
+++ src/Requests/BaseRequest.php
@@ -10,39 +10,35 @@ use ReflectionProperty;
 
 abstract class BaseRequest
 {
-    /** @var array<string, string>|null */
-    protected static ?array $propertySetters = null;
-
     /**
-     * Magic method to handle dynamic setters like setFoo($value).
-     *
-     * @throws ErrorException
+     * @throws \ErrorException
      */
-    public function __call(string $name, array $arguments): static
+    public function __call(string $name, mixed $arguments): static
     {
-        // Cache the map of setMethodName => propertyName
-        if (static::$propertySetters === null) {
-            static::$propertySetters = [];
-            $reflection = new ReflectionClass(static::class);
+        $propertyNames = [];
 
-            foreach ($reflection->getProperties(ReflectionProperty::IS_PUBLIC) as $property) {
-                if (! $property->isStatic()) {
-                    $field = $property->getName();
-                    static::$propertySetters['set'.ucfirst($field)] = $field;
-                }
+        foreach (
+            (new ReflectionClass(static::class))
+                ->getProperties(ReflectionProperty::IS_PUBLIC) as $reflectionProperty
+        ) {
+            if ($reflectionProperty->isStatic()) {
+                continue;
             }
+
+            $field = $reflectionProperty->getName();
+
+            $propertyNames['set'.ucfirst($field)] = $field;
         }
 
-        if (! isset(static::$propertySetters[$name])) {
-            throw new ErrorException(sprintf('Call to undefined method %s::%s()', static::class, $name));
+        if (! array_key_exists($name, $propertyNames)) {
+            throw new ErrorException(sprintf('%s::%s() not found.', static::class, $name));
         }
 
         if (count($arguments) !== 1) {
-            throw new ErrorException(sprintf('Method %s::%s() expects exactly 1 argument, %d given.', static::class, $name, count($arguments)));
+            throw new ErrorException(sprintf('Argument of %s::%s() is 1 expected.', static::class, $name));
         }
 
-        $property = static::$propertySetters[$name];
-        $this->$property = $arguments[0];
+        $this->{$propertyNames[$name]} = $arguments[0];
 
         return $this;
     }
