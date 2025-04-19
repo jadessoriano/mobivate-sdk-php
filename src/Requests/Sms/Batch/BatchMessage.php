86fbd0a35a72891ee778a02932c312eda9b7995d
jadessoriano <jadesiguasoriano@gmail.com>
2025-04-19 18:18:25 +0800

    format datetime

--- src/Requests/Sms/Batch/BatchMessage.php
+++ src/Requests/Sms/Batch/BatchMessage.php
@@ -4,11 +4,12 @@ declare(strict_types=1);
 
 namespace Jadessoriano\Mobivate\Requests\Sms\Batch;
 
+use DateTimeInterface;
 use Jadessoriano\Mobivate\Requests\BaseRequest;
 
 /**
  * @method BatchMessage setMessages(array $messages = [])
- * @method BatchMessage setScheduleDateTime(?string $scheduleDateTime = null)
+ * @method BatchMessage setScheduleDateTime(string|DateTimeInterface|null $scheduleDateTime = null)
  * @method BatchMessage setShortenUrls(bool $shortenUrls = false)
  * @method BatchMessage setRouteId(string $routeId = 'mglobal')
  */
@@ -16,8 +17,17 @@ class BatchMessage extends BaseRequest
 {
     public function __construct(
         public array $messages = [],
-        public ?string $scheduleDateTime = null,
+        public string|DateTimeInterface|null $scheduleDateTime = null,
         public bool $shortenUrls = false,
         public string $routeId = 'mglobal',
     ) {}
+
+    public function toArray(): array
+    {
+        if ($this->scheduleDateTime instanceof DateTimeInterface) {
+            $this->scheduleDateTime = $this->scheduleDateTime->format(DATE_ATOM);
+        }
+
+        return parent::toArray();
+    }
 }
